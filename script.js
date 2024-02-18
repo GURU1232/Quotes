d1aa985c7852c52440efa71b88c46810b2a21652
Guruprasath k <guruprasathk2020@gmail.com>
2024-02-18 13:44:29 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const quote = document.querySelector(".quote");
+const btn = document.querySelector(".quotebtn")
+
+function getData(){
+    fetch("https://api.kanye.rest/")
+    .then((res) => res.json())
+    .then((data)=>{
+        quote.innerHTML ="";
+        quote.innerHTML =`<h2 class = "bg-sucess bg-gradient text-while text-center">Quote</h2>
+        <p class = "ms-2 fs-4">${data.quote}</p>`;
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
+    btn.addEventListener("click",getData);
+}
